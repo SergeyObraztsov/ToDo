2b532fa35269cd2bfbd262ddae96f0419980dcb0
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-11-17 16:13:58 +0300

    styles

---
+++ src/components/search-bar/search-bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search-bar.css';
+
+const SearchBar = () => {
+    return (
+        <input type="text"
+            className="form-control search-input"
+            placeholder="Type here to search" />
+    );
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import TodoList from './components/todo-list';
-import SearchBar from './components/search-bar';
+import AppHeader from './components/app-header/app-header';
+import TodoList from './components/todo-list/todo-list';
+import SearchBar from './components/search-bar/search-bar';
+import ItemStatusFilter from './components/item-status-filter/item-status-filter';
 
+import './index.css';
 
 const App = () => {
     
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1 },
+        {label: 'Make React app', important: true, id: 2 },
+        {label: 'Have a launch', important: false, id: 3 }
+    ];
+
     return (
-        <div>
-            <AppHeader />
-            <SearchBar />
-            <TodoList />
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchBar />
+                <ItemStatusFilter />
+            </div>
+            <TodoList todos={todoData} />
         </div>
     );
 };
